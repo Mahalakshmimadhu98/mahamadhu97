8a627a60e198f5aba418ad70b8d001eb01ad347c
Mahalakshmimadhu98 <31218984+Mahalakshmimadhu98@users.noreply.github.com>
2017-09-05 23:05:12 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str1[20],st2r[20];
+printf("Enter a string");
+scanf("%s",&str1);
+str2=strrev(str1);
+printf("Reversed string:%s",str2);
+}
