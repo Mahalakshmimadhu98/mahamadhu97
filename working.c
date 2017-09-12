59cdcad7a43ac9acac643b0605cd39aded1378f9
Mahalakshmimadhu98 <31218984+Mahalakshmimadhu98@users.noreply.github.com>
2017-09-12 15:59:18 +0530

    Update working.c

--- working.c
+++ working.c
@@ -8,18 +8,25 @@ scanf("%s",&str);
 switch(str)
 {
 case "monday":printf("Working day");
+               return TRUE;
                break;
 case "tuesday":printf("Working day");
+                  return TRUE;
               break;
 case "wednesday":printf("Working day");
+                  return TRUE;
                  break;
 case "thursday":printf("Working day");
+                return TRUE;
                 break;
 case "friday":printf("Working day");
+                 return TRUE;
                break;
 case "saturday":printf("working day");
+                return TRUE;
                 break;
 case "sunday":printf("Holiday!!!");
+                 return FALSE;
                break;
 default :printf("Enter only days name");
 }
