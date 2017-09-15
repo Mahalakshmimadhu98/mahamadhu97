56746032c2994a3fc3ac7081c6722eebefd824e6
Mahalakshmimadhu98 <31218984+Mahalakshmimadhu98@users.noreply.github.com>
2017-09-15 22:44:03 +0530

    Create fibonaci.c

---
+++ fibonaci.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int f=0,s=1,n, i=0,num;
+printf("Enter a number");
+scanf("%d",&num);
+printf("series :%d%d",f,s);
+do()
+{
+n=f+s;
+f=s;
+s=n;
+printf("%d",n);
+}while(i<=n);
+}
