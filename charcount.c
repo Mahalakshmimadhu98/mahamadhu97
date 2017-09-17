8baf3aa816643ce840bb3940f4df9ccc1088e1cc
Mahalakshmimadhu98 <31218984+Mahalakshmimadhu98@users.noreply.github.com>
2017-09-17 20:01:48 +0530

    Create charcount.c

---
+++ charcount.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char s1[100];
+int i;
+printf("Enter a line of string");
+scanf("%s",&s1);
+i=strlen(s1);
+printf("Num of characters: %d",i);
+}
