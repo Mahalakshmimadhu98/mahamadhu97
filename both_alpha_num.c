0ba4a4a34f74759386c8de20845679aaa9161898
Mahalakshmimadhu98 <31218984+Mahalakshmimadhu98@users.noreply.github.com>
2018-01-21 11:19:00 +0530

    Update both_alpha_num.c

--- both_alpha_num.c
+++ both_alpha_num.c
@@ -4,7 +4,7 @@ void main()
 char st[30];
 printf("Enter a string:\n");
 scanf("%s",st);
-if((st<='a')||(st<='z')&&(st<=0)||(st>=9))
+if(((st<='a')||(st<='z'))&&((st<=0)||(st>=9)))
 {
 printf("yes");
 }
