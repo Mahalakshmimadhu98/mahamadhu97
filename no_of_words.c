cd172fd9efa79974449f2e54bf39075259a2d928
Mahalakshmimadhu98 <31218984+Mahalakshmimadhu98@users.noreply.github.com>
2018-01-21 12:28:03 +0530

    Create no_of_words.c

---
+++ no_of_words.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char sen[50];
+int i=0,count=1,j;
+printf("\n Enter a sentence");
+scanf("%s",sen);
+i=strlen(sen);
+for(j=0;j<n;j++)
+{
+if(sen[j]==" ")
+{
+count++;
+}
+}
+printf("no.of.words%d",count);
+}
