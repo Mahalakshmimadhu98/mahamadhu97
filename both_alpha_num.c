3665a5b15a4a47bfa7feb9d9d3d4e4de2557107c
Mahalakshmimadhu98 <31218984+Mahalakshmimadhu98@users.noreply.github.com>
2018-01-21 11:13:27 +0530

    Create both_alpha_num.c

---
+++ both_alpha_num.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char st[30];
+printf("Enter a string:\n");
+scanf("%s",st);
+if((st<='a')||(st<='z')&&(st<=0)||(st>=9))
+{
+printf("yes");
+}
+else
+{
+printf("No");
+}
+}
