57e292ec7cfbcfe8af7602c2c028d185a7783e59
Mahalakshmimadhu98 <31218984+Mahalakshmimadhu98@users.noreply.github.com>
2018-01-23 15:44:05 +0530

    Create add_check_eve_even.c

---
+++ add_check_eve_even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n1,n2,sum=0;
+printf("\n Enter 2 numbers");
+scanf("%d%d",&a,&b);
+sum=a+b;
+if(sum%2==0)
+{
+printf("\n Odd");
+}
+else
+{
+printf("\n Even");
+}
+}
