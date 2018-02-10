8a94a0cf7a260db2d65253375c932e89d9e34110
Mahalakshmimadhu98 <31218984+Mahalakshmimadhu98@users.noreply.github.com>
2018-02-10 09:02:08 +0000

    Create kabali_ninja.c

---
+++ kabali_ninja.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int kn,on,d=0;
+printf("\nEnter no.of ninjas in kabali team & opponent team:");
+scanf("%d%d",&kn,on);
+d=kn-on;
+printf("\nDifference between ninjas:%d",d);
+}
