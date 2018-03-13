b99cb07294fa8b273be8b4d03a25f61f32d20585
Mahalakshmimadhu98 <31218984+Mahalakshmimadhu98@users.noreply.github.com>
2018-03-13 16:53:54 +0530

    Create remove_all_space.c

---
+++ remove_all_space.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char s[20],s1[20];
+    int i=0,flag=0,j=0,l;
+    printf("\nEnter a string:");
+    gets(s);
+    l=strlen(s);
+    while(i<l)
+    {
+     if(s[i]!=' ')
+     {
+         s1[j]=s[i];
+         j++;
+         
+     }
+     else
+     {
+         s1[j]=s[i+1];
+         j++;
+         i++;
+     }
+     i++;
+    }
+    s1[j]='\0';
+  
+    printf("\nString after removal : %s",s1);
+    
+      return 0;
+}
