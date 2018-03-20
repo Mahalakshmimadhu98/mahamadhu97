6ae8477b8d4dc92b0ea16052ff97a7cd20e2c05c
Mahalakshmimadhu98 <31218984+Mahalakshmimadhu98@users.noreply.github.com>
2018-03-20 16:47:18 +0530

    Create odd_factors.c

---
+++ odd_factors.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int n,i,k=0;
+printf("\n Enter a number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+    k=n%i;
+    if(k==0)
+    {
+      if(i%2!=0)
+       {
+       printf("%d\t",i);
+       }
+    }
+}
+}
